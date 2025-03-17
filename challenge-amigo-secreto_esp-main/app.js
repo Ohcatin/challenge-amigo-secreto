bc164ef0935ab339682822b587171c3d67b64700
Ohcatin <catalinajaviera.ordenes@gmail.com>
2025-03-17 07:46:56 -0300

    Se logra funcion para agregar amigos, funcion para actualizar la lista de amigos y que al ingresar un amigo el campo de texto se limpie

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,3 +1,24 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos = []; //mi array vacio 
+const inputAmigo = document.getElementById('amigo'); //para tomar texto
+const arrayAmigos = []; //array vacio
+const ulListaAmigos = document.getElementById('listaAmigos'); 
 
+function agregarAmigo() {
+    if (inputAmigo.value == ''){
+        alert('Por favor, inserte un nombre.') //mensaje que sale si no se escribe nada
+    }
+    arrayAmigos.push(inputAmigo.value);
+    inputAmigo.value = '';
+    //console.log(listaAmigos);
+//ulListaAmigos.innerHTML += `<li>${inputAmigo.value}</li>`; 
+actualizarLista();
+}
+
+function actualizarLista() {
+    ulListaAmigos.innerHTML = ''; // Limpiar la lista antes de actualizar
+
+    for (let i = 0; i < arrayAmigos.length; i++) {
+        const nuevoAmigo = document.createElement('li');
+        nuevoAmigo.textContent = arrayAmigos[i];
+        ulListaAmigos.appendChild(nuevoAmigo);
+}
+}
